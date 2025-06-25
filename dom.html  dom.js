c2764469401275511bd5148153b262a39f3eb45a
Mahesh2000v <bandimaheshchandra@gmail.com>
2025-06-25 15:15:47 +0530

    Create dom.html  dom.js

---
+++ dom.html  dom.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>html file</title>
+</head>
+<body>
+    <h1 id="mrcet">mahiiii </h1>
+<p id="mr" >data scienec</p>
+<div class="id"> THIMBRAD </div>
+<div class="id"> RUFFFFFII</div>
+<div class="id"> PREETHIIII</div>
+<div class="id"> ALINA RAI</div>
+<script src="dom.js"></script>
+</body>
+</html>
+
+
+
+document.title="java.js"
+document.getElementById("mrcet").innerHTML="hello,world";
+document.getElementById("mrcet").style.color="green";
+document.getElementById("mr").style.color="red";
+document.getElementById("mr").style.fontSize ="30px";
+document.getElementsByClassName("id")[0].style.color="red";
+document.getElementsByClassName("id")[1].style.color="green";
+document.getElementsByClassName("id")[2].style.color="bule";
+document.getElementsByClassName("id")[3].style.color="yellow";
+document.getElementsByClassName("id")[0].style.fontSize="50px";
+document.getElementsByClassName("id")[1].style.fontSize="60px";
+document.getElementsByClassName("id")[2].style.fontSize="70px";
+document.getElementsByClassName("id")[3].style.fontSize="80px";
+document.body.style.backgroundColor="orange";
